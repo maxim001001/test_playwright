5331e1101a3de4df5768d80ea5e86c0ff0a5a541
maxim001001 <161547021+maxim001001@users.noreply.github.com>
2024-02-29 05:06:55 +0000

    fix beforeEach hook

--- tests/UserAuth.spec.ts
+++ tests/UserAuth.spec.ts
@@ -6,32 +6,34 @@
 import { test, expect, Page } from "@playwright/test";
 import { TEST_URL } from "./setTestUrl";
 
-test.beforeEach(async ({ page }: Page) => {
-  await page.goto(TEST_URL);
-  await page.getByText("Авторизоваться").click();
-});
+test.describe("Test Registration Form", () => {
+  test.beforeEach(async ({ page }: Page) => {
+    await page.goto(TEST_URL);
+    await page.getByText("Авторизоваться").click();
+  });
 
-test("test write wrong data", async ({ page }: Page) => {
-  await expect(page.getByTestId("auth-form")).toBeVisible();
-  await page.getByPlaceholder("Почта").fill("test");
-  await page.getByPlaceholder("Пароль").fill("test");
-  const buttonLocator = await page.getByText("Отправить");
-  const isButtonDisabled = await buttonLocator.evaluate(
-    (button: HTMLButtonElement) => button.disabled,
-  );
-  expect(isButtonDisabled).toBe(true);
-  await expect(page).toHaveURL(TEST_URL);
-});
+  test("test write wrong data", async ({ page }: Page) => {
+    await expect(page.getByTestId("auth-form")).toBeVisible();
+    await page.getByPlaceholder("Почта").fill("test");
+    await page.getByPlaceholder("Пароль").fill("test");
+    const buttonLocator = await page.getByText("Отправить");
+    const isButtonDisabled = await buttonLocator.evaluate(
+      (button: HTMLButtonElement) => button.disabled,
+    );
+    expect(isButtonDisabled).toBe(true);
+    await expect(page).toHaveURL(TEST_URL);
+  });
 
-test("test write right data", async ({ page }: Page) => {
-  await expect(page.getByTestId("auth-form")).toBeVisible();
-  await page.getByPlaceholder("Почта").fill("admin@gmail.com");
-  await page.getByPlaceholder("Пароль").fill("12345");
-  await page.getByText("Отправить").click();
-  await expect(page.getByText("У вас есть доступ на сайт")).toBeVisible();
-  await expect(page).toHaveURL(`${TEST_URL}` + "dashboard");
-  await page.getByText("Выйти").click();
-  await expect(page).toHaveURL(TEST_URL);
-  await expect(page.getByText("Авторизоваться")).toBeVisible();
-  await expect(page.getByTestId("auth-form")).toBeVisible();
+  test("test write right data", async ({ page }: Page) => {
+    await expect(page.getByTestId("auth-form")).toBeVisible();
+    await page.getByPlaceholder("Почта").fill("admin@gmail.com");
+    await page.getByPlaceholder("Пароль").fill("12345");
+    await page.getByText("Отправить").click();
+    await expect(page.getByText("У вас есть доступ на сайт")).toBeVisible();
+    await expect(page).toHaveURL(`${TEST_URL}` + "dashboard");
+    await page.getByText("Выйти").click();
+    await expect(page).toHaveURL(TEST_URL);
+    await expect(page.getByText("Авторизоваться")).toBeVisible();
+    await expect(page.getByTestId("auth-form")).toBeVisible();
+  });
 });
